3d65112776bdb108dd3e40970f0d3ff4bd160b85
Douglas Jose <douglasjose3@gmail.com>
2024-01-10 23:12:11 -0800

    Criando o hero e importando a font Avenir

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,6 @@
 const gulp = require('gulp');
 const sass = require('gulp-sass')(require('sass'));
+const imagemin = require('gulp-imagemin');
 
 function styles() { 
     return gulp.src('./src/styles/*.scss') // onde vamos pegar os arquivos fontes
@@ -7,7 +8,13 @@ function styles() {
         .pipe(gulp.dest('./dist/css')); // pasta de destino
 }
 
-exports.default = styles; // invocar a função
+function images() {
+    return gulp.src('./src/images/**/*') // para pegar todas as imagens
+    .pipe(imagemin())
+    .pipe(gulp.dest('./dist/images'));
+}
+
+exports.default = gulp.parallel(styles, images); // invocar as funções
 exports.watch = function() { // para não ter que ficar colocando o tempo todo o npm rum build
     gulp.watch('./src/styles/*.scss', gulp.parallel(styles))
 }
