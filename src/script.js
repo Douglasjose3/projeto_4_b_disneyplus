502f9a569d5d32278a8dbccde8d3b8a3fceddc48
Douglas Jose <douglasjose3@gmail.com>
2024-01-21 23:23:31 -0800

    Header finalizado

--- src/script.js
+++ src/script.js
@@ -1,7 +1,21 @@
 document.addEventListener('DOMContentLoaded', function() {
     const buttons = document.querySelectorAll('[data-tab-button]'); // pegar todos os botões
     const questions = document.querySelectorAll('[data-faq-question]');
+
+    const heroSelection = document.querySelector('.hero');
+    const alturaHero = heroSelection.clientHeight;
+
+    window.addEventListener('scroll', function() {
+        const posicaoAtual = window.scrollY; // busca a altura para o conteúdo aparecer
+
+        if (posicaoAtual < alturaHero) {
+            ocultaElementosHeader();
+        } else {
+            exibeElementosDoHeader();
+        }
+    })
     
+    // Seção de atrações, programação das abas
     for (let i = 0; i < buttons.length; i++) { // evento ao clique dos botões
         buttons[i].addEventListener('click', function(botao) { // procurar e acessar o target do botão no console (inspect)
             // console.log(botao.target.dataset.tabButton); // acessando o target do botão
@@ -13,11 +27,22 @@ document.addEventListener('DOMContentLoaded', function() {
             botao.target.classList.add('shows__tabs__button--is--active');
         })
 
+        // Seção FAQ, accordion
         for (let i = 0; i < questions.length; i++) {
             questions[i].addEventListener('click', abreOuFechaResposta);
         }
     }
 })
+
+function ocultaElementosHeader() {
+    const header = document.querySelector('header');
+    header.classList.add('header--is--hidden');
+}
+
+function exibeElementosDoHeader() {
+    const header = document.querySelector('header');
+    header.classList.remove('header--is--hidden');
+}
 
 function abreOuFechaResposta(elemento) {
     const classe= 'faq__questions__item--is--open';
