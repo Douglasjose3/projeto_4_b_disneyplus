8c18f46059ba123e31ae86bf83b4f6414517ff2c
Douglas Jose <douglasjose3@gmail.com>
2024-01-22 16:59:31 -0800

    Configurando o uglify

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,13 @@
 const gulp = require('gulp');
 const sass = require('gulp-sass')(require('sass'));
 const imagemin = require('gulp-imagemin');
+const uglify = require('gulp-uglify');
+
+function scripts() { // para minificar o html
+    return gulp.src('./src/scripts/*.js')
+        .pipe(uglify())
+        .pipe(gulp.dest('./dist/js'));
+}
 
 function styles() { 
     return gulp.src('./src/styles/*.scss') // onde vamos pegar os arquivos fontes
@@ -14,7 +21,8 @@ function images() {
     .pipe(gulp.dest('./dist/images'));
 }
 
-exports.default = gulp.parallel(styles, images); // invocar as funções
+exports.default = gulp.parallel(styles, images, scripts); // invocar as funções
 exports.watch = function() { // para não ter que ficar colocando o tempo todo o npm rum build
     gulp.watch('./src/styles/*.scss', gulp.parallel(styles))
+    gulp.watch('./src/scripts/*.js', gulp.parallel(scripts))
 }
